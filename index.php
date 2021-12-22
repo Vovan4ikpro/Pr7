7c57cafa0e4c38aa6251d1985c418535d6d9a971
Vova <v2110vova@gmail.com>
2021-12-23 01:27:17 +0200

    update

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+session_start();
+require_once 'config/db.php';
+require_once 'route/web.php';
+
+
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+
+//define controller and action
+$controllerName = isset($_GET['controller']) ? $_GET['controller'] : 'index';
+$actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+//завантажуємо об’єкт роутінга
+$routing = new Route();
+//завантажуємо об'єкт моделі
+$db = new Db();
+
+$routing->loadPage($db, $controllerName, $actionName);
